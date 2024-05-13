b464bc7a56f3edf1e70a250d70fd1958f24882fb
Seung Woon Yu <94117187+oho3473@users.noreply.github.com>
2024-05-13 16:46:34 +0900

    feat: graphics interface 추가

---
+++ VPGraphics/IGraphics.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+namespace Graphics
+{
+	class Interface
+	{
+	public:
+		Interface();
+		virtual ~Interface();
+
+		virtual void Initialize() abstract;
+		virtual void Update(double dt) abstract;
+		virtual void Finalize() abstract;
+
+		virtual void Render() abstract;
+
+		virtual void OnResize() abstract;
+
+	private:
+
+	};
+}
