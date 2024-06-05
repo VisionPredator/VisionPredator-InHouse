529117daaeef97c3fbaddc513658b24e602f5659
Seung Woon Yu <usw3473@gmail.com>
2024-06-05 18:35:59 +0900

    fix: release 종속성 변경하면서 디버그 환경에서만 돌아가는 shader reflection 주석처리

--- VPGraphics/BasePS.hlsl
+++ VPGraphics/BasePS.hlsl
@@ -11,7 +11,7 @@ struct PS_INPUT
 
 
 
-float3 main(PS_INPUT input) : SV_TARGET
+float4 main(PS_INPUT input) : SV_TARGET
 {
     return input.color;
     //return float4(0.f, 1.0f, 0.f, 0.f);
--- VPGraphics/VertexShader.cpp
+++ VPGraphics/VertexShader.cpp
@@ -129,90 +129,90 @@ VertexShader::VertexShader(std::shared_ptr<Device>device, VERTEXFILTER kind_of_v
 
 VertexShader::VertexShader(const std::shared_ptr<Device>& device, const std::wstring& filename, const int& a)
 {
-	DWORD shaderFlag = D3DCOMPILE_ENABLE_STRICTNESS;
-#ifdef _DEBUG
-	shaderFlag |= D3DCOMPILE_DEBUG;
-	shaderFlag |= D3DCOMPILE_SKIP_OPTIMIZATION;
-#endif
-
-	D3D_SHADER_MACRO* pDefines = nullptr;
-	const std::string& entryPoint = "main";
-	const std::string& shaderModel = "vs_5_0";
-
-	Microsoft::WRL::ComPtr<ID3DBlob> blob;
-	HR_CHECK(D3DCompileFromFile(filename.c_str(), pDefines, D3D_COMPILE_STANDARD_FILE_INCLUDE, entryPoint.c_str(), shaderModel.c_str(),
-		shaderFlag, 0, &blob, &blob));
-
-	HR_CHECK(device->Get()->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, &m_VS));
-
-
-	/// 이제 인풋 레이아웃 만들기
-	ID3D11ShaderReflection* pReflector = nullptr;
-
-	// Create Reflector..
-	D3DReflect(blob->GetBufferPointer(), blob->GetBufferSize(), IID_ID3D11ShaderReflection, (void**)&pReflector);
-
-	// Shader Refection
-	D3D11_SHADER_DESC shaderDesc;
-	pReflector->GetDesc(&shaderDesc);
-
-	/// Input Layout Reflection
-	// Shader Input Layout..
-	std::vector<D3D11_INPUT_ELEMENT_DESC> inputLayoutDesc;
-	for (unsigned inputIndex = 0; inputIndex < shaderDesc.InputParameters; inputIndex++)
-	{
-		D3D11_SIGNATURE_PARAMETER_DESC paramDesc;
-		pReflector->GetInputParameterDesc(inputIndex, &paramDesc);
-
-		// Shader Input Data를 기반으로 생성..
-		D3D11_INPUT_ELEMENT_DESC elementDesc;
-		elementDesc.SemanticName = paramDesc.SemanticName;
-		elementDesc.SemanticIndex = paramDesc.SemanticIndex;
-		elementDesc.InputSlot = 0;
-		elementDesc.AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-		elementDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-		elementDesc.InstanceDataStepRate = 0;
-
-		// Shader Data 기반으로 DXGI format 설정..
-		if (paramDesc.Mask == 1)
-		{
-			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32_UINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32_SINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32_FLOAT;
-		}
-		else if (paramDesc.Mask <= 3)
-		{
-			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32G32_UINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32G32_SINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32G32_FLOAT;
-		}
-		else if (paramDesc.Mask <= 7)
-		{
-			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32_UINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32_SINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
-		}
-		else if (paramDesc.Mask <= 15)
-		{
-			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32A32_UINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32A32_SINT;
-			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-		}
-
-		// 현 InputLayout 데이터 삽입..
-		inputLayoutDesc.push_back(elementDesc);
-	}
-
-	// Shader InputLayout 생성..
-	HR_CHECK(
-		device->Get()->CreateInputLayout(
-			&inputLayoutDesc[0], 
-			(UINT)inputLayoutDesc.size(),
-			blob->GetBufferPointer(), 
-			blob->GetBufferSize(),
-			&m_inputLayout
-		);
-	);
+//	DWORD shaderFlag = D3DCOMPILE_ENABLE_STRICTNESS;
+//#ifdef _DEBUG
+//	shaderFlag |= D3DCOMPILE_DEBUG;
+//	shaderFlag |= D3DCOMPILE_SKIP_OPTIMIZATION;
+//#endif
+//
+//	D3D_SHADER_MACRO* pDefines = nullptr;
+//	const std::string& entryPoint = "main";
+//	const std::string& shaderModel = "vs_5_0";
+//
+//	Microsoft::WRL::ComPtr<ID3DBlob> blob;
+//	HR_CHECK(D3DCompileFromFile(filename.c_str(), pDefines, D3D_COMPILE_STANDARD_FILE_INCLUDE, entryPoint.c_str(), shaderModel.c_str(),
+//		shaderFlag, 0, &blob, &blob));
+//
+//	HR_CHECK(device->Get()->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, &m_VS));
+//
+//
+//	/// 이제 인풋 레이아웃 만들기
+//	ID3D11ShaderReflection* pReflector = nullptr;
+//
+//	// Create Reflector..
+//	D3DReflect(blob->GetBufferPointer(), blob->GetBufferSize(), IID_ID3D11ShaderReflection, (void**)&pReflector);
+//
+//	// Shader Refection
+//	D3D11_SHADER_DESC shaderDesc;
+//	pReflector->GetDesc(&shaderDesc);
+//
+//	/// Input Layout Reflection
+//	// Shader Input Layout..
+//	std::vector<D3D11_INPUT_ELEMENT_DESC> inputLayoutDesc;
+//	for (unsigned inputIndex = 0; inputIndex < shaderDesc.InputParameters; inputIndex++)
+//	{
+//		D3D11_SIGNATURE_PARAMETER_DESC paramDesc;
+//		pReflector->GetInputParameterDesc(inputIndex, &paramDesc);
+//
+//		// Shader Input Data를 기반으로 생성..
+//		D3D11_INPUT_ELEMENT_DESC elementDesc;
+//		elementDesc.SemanticName = paramDesc.SemanticName;
+//		elementDesc.SemanticIndex = paramDesc.SemanticIndex;
+//		elementDesc.InputSlot = 0;
+//		elementDesc.AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+//		elementDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+//		elementDesc.InstanceDataStepRate = 0;
+//
+//		// Shader Data 기반으로 DXGI format 설정..
+//		if (paramDesc.Mask == 1)
+//		{
+//			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32_UINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32_SINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32_FLOAT;
+//		}
+//		else if (paramDesc.Mask <= 3)
+//		{
+//			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32G32_UINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32G32_SINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32G32_FLOAT;
+//		}
+//		else if (paramDesc.Mask <= 7)
+//		{
+//			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32_UINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32_SINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+//		}
+//		else if (paramDesc.Mask <= 15)
+//		{
+//			if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32A32_UINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32) elementDesc.Format = DXGI_FORMAT_R32G32B32A32_SINT;
+//			else if (paramDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32) elementDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+//		}
+//
+//		// 현 InputLayout 데이터 삽입..
+//		inputLayoutDesc.push_back(elementDesc);
+//	}
+//
+//	// Shader InputLayout 생성..
+//	HR_CHECK(
+//		device->Get()->CreateInputLayout(
+//			&inputLayoutDesc[0], 
+//			(UINT)inputLayoutDesc.size(),
+//			blob->GetBufferPointer(), 
+//			blob->GetBufferSize(),
+//			&m_inputLayout
+//		);
+//	);
 }
 
 VertexShader::~VertexShader()
