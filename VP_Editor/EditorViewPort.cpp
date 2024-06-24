d5c9423f9850c661d3d6c0aa582319615eba5387
DongHyu <ehdgb97@gmail.com>
2024-06-24 19:03:44 +0900

    feat: RenderMode 추가

--- VP_Editor/EditorViewPort.cpp
+++ VP_Editor/EditorViewPort.cpp
@@ -36,27 +36,84 @@ void EditorViewPort::PlayingImGui()
 
 void EditorViewPort::EditingImGui()
 {  
-	
+	float FrameHeight = ImGui::GetFrameHeight();
 	auto current = ImGui::GetCurrentContext();
-	auto pos = ImVec2{ current->CurrentWindow->Pos.x, current->CurrentWindow->Pos.y + 25.f };
-	auto size = ImVec2{ current->CurrentWindow->Size.x, current->CurrentWindow->Size.y - 25.f };
+	auto pos = ImVec2{ current->CurrentWindow->Pos.x, current->CurrentWindow->Pos.y + FrameHeight };
+	auto size = ImVec2{ current->CurrentWindow->Size.x, current->CurrentWindow->Size.y - FrameHeight };
 	auto idealSize = ImVec2(size.x, size.y);
 	auto maxpos = ImVec2(pos.x + idealSize.x, pos.y + idealSize.y);
 	auto borderSize = current->CurrentWindow->WindowBorderSize;
 
+	//뷰포트 설정된 것 출력하기
+	switch (m_CurrentRenderMode)
+	{
+	case EditorViewPort::RENDERMODE::Albedo:
+		EditViewPortImGui(L"Albedo", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::Normal:
+		EditViewPortImGui(L"Normal", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::Position:
+		EditViewPortImGui(L"Position", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::Metalic:
+		EditViewPortImGui(L"Metalic", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::Roughness:
+		EditViewPortImGui(L"Roughness", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::Emissive:
+		EditViewPortImGui(L"Emissive", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::GBuffer:
+		EditViewPortImGui(L"GBuffer", pos, maxpos);
+		break;
+	case EditorViewPort::RENDERMODE::IMGUI:
+		EditViewPortImGui(L"IMGUI", pos, maxpos);
+		break;
+	default:
+		break;
+	}
 
-	ImGui::GetWindowDrawList()->AddImage(
-		(void*)m_Graphics->GetSRV(L"IMGUI"),
-		ImVec2(pos.x, pos.y),
-		ImVec2(maxpos.x, maxpos.y));
+
+
+
+
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_None | ImGuiWindowFlags_NoTitleBar |ImGuiWindowFlags_NoTitleBar;
+	ImGuiChildFlags child_flags = ImGuiWindowFlags_None | ImGuiChildFlags_Border | ImGuiChildFlags_AutoResizeX | ImGuiChildFlags_AutoResizeY | ImGuiChildFlags_FrameStyle;
+	ImVec2 childWindowPos = ImVec2(maxpos.x - 110, pos.y);
+	ImGui::SetNextWindowPos(childWindowPos);
+	ImGui::PushStyleVar(ImGuiStyleVar_ChildRounding, 10.0f);
+	ImGui::BeginChild("Tools", ImVec2{ 110,0 }, child_flags, window_flags);
+	if (ImGui::RadioButton("Albedo", m_CurrentRenderMode == RENDERMODE::Albedo))
+		m_CurrentRenderMode = RENDERMODE::Albedo;
+	if (ImGui::RadioButton("Normal", m_CurrentRenderMode == RENDERMODE::Normal))
+		m_CurrentRenderMode = RENDERMODE::Normal;
+	if (ImGui::RadioButton("Position", m_CurrentRenderMode == RENDERMODE::Position))
+		m_CurrentRenderMode = RENDERMODE::Position;
+	if (ImGui::RadioButton("Metalic", m_CurrentRenderMode == RENDERMODE::Metalic))
+		m_CurrentRenderMode = RENDERMODE::Metalic;
+	if (ImGui::RadioButton("Roughness", m_CurrentRenderMode == RENDERMODE::Roughness))
+		m_CurrentRenderMode = RENDERMODE::Roughness;
+	if (ImGui::RadioButton("Emissive", m_CurrentRenderMode == RENDERMODE::Emissive))
+		m_CurrentRenderMode = RENDERMODE::Emissive;
+	if (ImGui::RadioButton("GBuffer", m_CurrentRenderMode == RENDERMODE::GBuffer))
+		m_CurrentRenderMode = RENDERMODE::GBuffer;
+	if (ImGui::RadioButton("IMGUI", m_CurrentRenderMode == RENDERMODE::IMGUI))
+		m_CurrentRenderMode = RENDERMODE::IMGUI;
+	ImGui::EndChild();
+	ImGui::PopStyleVar();
+
 	ImGuizmoRender();
+}
 
+void EditorViewPort::EditViewPortImGui(std::wstring mode, ImVec2 pos, ImVec2 maxPos)
+{
+	ImGui::GetWindowDrawList()->AddImage((void*)m_Graphics->GetSRV(mode), ImVec2(pos.x, pos.y), ImVec2(maxPos.x, maxPos.y));
 }
 
 void EditorViewPort::ImGuizmoRender()
 {
-
-
 	if (!m_SceneManager->HasEntity(HierarchySystem::m_SelectedEntityID))
 		return;
 
@@ -89,8 +146,4 @@ void EditorViewPort::UsingImGuizmo()
 {
 	if (!ImGuizmo::IsUsing())
 		return;
-
-
-
-
 }
