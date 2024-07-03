4c76d60ab4cb7d5047fea056b485a7a9f184d798
DongHyu <ehdgb97@gmail.com>
2024-07-03 17:43:52 +0900

    feat: ColliderManager 구조 생각해보기

--- PhysxEngine/PhysxColliManager.h
+++ PhysxEngine/PhysxColliManager.h
@@ -1,8 +1,12 @@
 #pragma once
+#include "VPPhysicsStructs.h"
 class PhysxColliManager
 {
 public: 
 	PhysxColliManager();
 	~PhysxColliManager();
+
+
+	std::vector<VPPhysics::CollisionData> m_CollisionData;
 };
 
