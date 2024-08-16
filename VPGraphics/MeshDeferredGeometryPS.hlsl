cca26eb0746533da8845c4cc0670ef36162c2687
Seung Woon Yu <usw3473@gmail.com>
2024-08-16 15:48:37 +0900

    feat : lightmap 맵핑

--- VPGraphics/MeshDeferredGeometryPS.hlsl
+++ VPGraphics/MeshDeferredGeometryPS.hlsl
@@ -6,8 +6,57 @@
 ///
 /// </summary>
 
+
+/*
+struct VS_OUTPUT
+{
+    float4 pos : SV_POSITION;
+    float4 posWorld : WORLDPOSITION;
+    float4 color : COLOR;
+    float4 normal : NORMAL;
+    float4 tangent : TANGENT;
+    float4 bitangent : BITANGENT;
+    float2 tex : TEXCOORD;
+    float2 lightuv : LIGHTMAPUV;
+};
+
+
+//Material
+cbuffer Material : register(b0)
+{
+    float4 AMRO;
+    float4 useNEO;
+    float4 albedo;
+    float metalness;
+    float roughness;
+    float ao; // Ambient Occlusion
+    float pad;
+    float4 lightmapdata; //index, offset(x,y),scale
+    float2 lightmaptiling; // x y
+};
 // Render to texture.
 // Using Multi Render Target.
+
+
+//TEXTURE
+Texture2D gAlbedo : register(t0);
+Texture2D gNormal : register(t1);
+Texture2D gPosition : register(t2);
+Texture2D gDepth : register(t3);
+Texture2D gMetalic : register(t4);
+Texture2D gRoughness : register(t5);
+Texture2D gAO : register(t6);
+Texture2D gEmissive : register(t7);
+Texture2D gOpacity : register(t8);
+Texture2D gLightMap : register(t9);
+
+//sampler
+SamplerState samLinear : register(s0);
+
+*/
+
+
+
 struct PS_OUTPUT
 {
     float4 Albedo : SV_Target0;
@@ -82,7 +131,7 @@ PS_OUTPUT main(VS_OUTPUT input) : SV_TARGET
     float x = (input.lightuv.x * lightmaptiling.x) + lightmapdata.y;
     float y = (input.lightuv.y * lightmaptiling.y) + lightmapdata.z;
 
-    float2 uv = float2(x, y);
+    float2 uv = float2(x, y);    
     
     output.LightMap = gLightMap.Sample(samLinear, uv);
     
