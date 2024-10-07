f592882d85c2f3b3374f6b48fc88973421c21377
DongHyu <ehdgb97@gmail.com>
2024-10-07 17:06:29 +0900

    feat: Searching 하이어라키 추가!

--- VP_Editor/HierarchySystem.h
+++ VP_Editor/HierarchySystem.h
@@ -15,6 +15,7 @@ public:
 	}
 
 	void ShowParentEntity(uint32_t entityID);
+	void ShowEntity(uint32_t entityID);
 	static uint32_t m_SelectedEntityID;
 	uint32_t m_RClickedEntityID = 0;
 	inline static bool IsItemDoubleClicked = false;
