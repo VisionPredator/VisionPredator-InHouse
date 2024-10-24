d085f5525e4f00d82ccb605d61bf0e99c5f73da4
heosumin518 <heosumin518@gmail.com>
2024-10-24 20:32:13 +0900

    Feat: Enemy 공격력 에디터에서 변환가능하게 연결해 놓음

---
+++ VisionpredatorProcess/EnemyIdleAttack.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "EnemyIdleAttack.h"
+
+void EnemyIdleAttack::Enter(const std::shared_ptr<Component>& component)
+{
+}
+
+void EnemyIdleAttack::Update(const std::shared_ptr<Component>& component, float deltaTime)
+{
+}
+
+void EnemyIdleAttack::Exit(const std::shared_ptr<Component>& component)
+{
+}
--- VisionpredatorProcess/EnemyMovementState.h
+++ VisionpredatorProcess/EnemyMovementState.h
@@ -1,24 +1,29 @@
 #pragma once
 #include "EnemyState.h"
 
-class EnemyJumpState;
+class EnemyAttackState;
 class EnemyIdleMovementState;
+class EnemyIdleAttack;
 class EnemyWalkState;
-class EnemyBackpedalState;
 class EnemyRunState;
+class EnemyJumpState;
+class EnemyBackpedalState;
 class EnemyHitReactionState;
+
 class EnemyMovementState : public virtual EnemyState
 {
 public:
-	virtual void Enter(const std::shared_ptr<Component>& component) override;
-	virtual void Update(const std::shared_ptr<Component>& component, float deltaTime) override;
-	virtual void Exit(const std::shared_ptr<Component>& component) override;
+	void Enter(const std::shared_ptr<Component>& component) override;
+	void Update(const std::shared_ptr<Component>& component, float deltaTime) override;
+	void Exit(const std::shared_ptr<Component>& component) override;
 
 public:
 	static EnemyIdleMovementState s_Idle;
+	static EnemyIdleAttack s_IdleAttack;
 	static EnemyWalkState s_Walk;
 	static EnemyRunState s_Run;
 	static EnemyJumpState s_Jumping;
 	static EnemyBackpedalState s_Backpedal;
-	static EnemyHitReactionState s_HitReaction;	// 애매한데.
+	static EnemyHitReactionState s_HitReaction;
+	static EnemyAttackState s_Attack;
 };
