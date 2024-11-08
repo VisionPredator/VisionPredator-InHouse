10a26ef94f0dfa1aab1b07c47bafcbf1bd650fa1
DongHyu <ehdgb97@gmail.com>
2024-11-08 16:21:25 +0900

    feat:player 커밋

--- VisionpredatorProcess/VisPredStructs.h
+++ VisionpredatorProcess/VisPredStructs.h
@@ -60,28 +60,29 @@ namespace VisPred
 		};
 		enum class PlayerAni
 		{
-			 ToAttack_Pistol	=0
-			,ToAttack_Rifle		=1
-			,ToAttack_ShotGun	=2
-			,ToIdle01_Pistol	=3
-			,ToIdle01_Rifle		=4
-			,ToIdle01_ShotGun	=5
-			,ToIdle02_Pistol	=6
-			,ToIdle02_Rifle		=7
-			,ToIdle02_ShotGun	=8
-			,Tohook_Sword		=9
-			,Tohook_Pistol		=10
-			,Tohook_Rifle		=11
-			,Tohook_ShotGun		=12
-			,Tointeraction		=13
-			,ToAttack1_Sword	=14
-			,ToAttack2_Sword	=15
-			,ToAttack3_Sword	=16
-			,ToIdle01_Sword		=17
-			,ToIdle02_Sword		=18
-			,ToThrow_Pistol		=19
-			,ToThrow_Rifle		=20
-			,ToThrow_ShotGun	=21
+			ToAttack_Pistol = 0
+			, ToAttack_Rifle = 1
+			, ToAttack_ShotGun = 2
+			, ToIdle01_Pistol = 3
+			, ToIdle01_Rifle = 4
+			, ToIdle01_ShotGun = 5
+			, ToIdle02_Pistol = 6
+			, ToIdle02_Rifle = 7
+			, ToIdle02_ShotGun = 8
+			, Tohook_Sword = 9
+			, Tohook_Pistol = 10
+			, Tohook_Rifle = 11
+			, Tohook_ShotGun = 12
+			, Tointeraction = 13
+			, ToAttack1_Sword = 14
+			, ToAttack2_Sword = 15
+			, ToAttack3_Sword = 16
+			, ToIdle01_Sword = 17
+			, ToIdle02_Sword = 18
+			, ToThrow_Pistol = 19
+			, ToThrow_Rifle = 20
+			, ToThrow_ShotGun = 21
+			, End = 22
 
 		};
 		enum class VPAni
@@ -93,6 +94,7 @@ namespace VisPred
 			, ToVP_jump
 			, ToVP_run
 			, ToVP_draw 
+			, End
 		};
 		enum class EnemyStates // 임시 이름. EnemyStates 로 교체
 		{
