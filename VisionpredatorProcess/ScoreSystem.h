b05cc13d30e36b3acb2acf7e8397eceb501a6fd5
DongHyu <ehdgb97@gmail.com>
2024-11-11 20:32:48 +0900

    feat: scoresystem커밋

---
+++ VisionpredatorProcess/ScoreSystem.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <System.h>
+#include "EventSubscriber.h"
+class ScoreSystem :
+	public System, public EventSubscriber,public IStartable
+{
+public: ScoreSystem(std::shared_ptr<SceneManager> scenemanager);
+
+	  
+	  void OnSpawnBestScore(std::any null = {});
+	  void OnEnemyKilled(std::any null = {});
+	  void OnHideScore(std::any null = {});
+	  void OnScoreToMiddle(std::any null = {});
+	  void OnEndingBestScore(std::any null = {});
+	  std::weak_ptr <Entity> m_Score;
+	  std::weak_ptr <Entity> m_BestScore;
+
+	  // IStartable을(를) 통해 상속됨
+	  void Initialize() override;
+	  void Start(uint32_t gameObjectId) override;
+	  void Finish(uint32_t gameObjectId) override;
+	  void Finalize() override;
+};
+
